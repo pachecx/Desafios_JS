f4c5d9bb13347ca6b2ef34488306e8d912106447
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-08-17 10:22:40 -0300

    up

--- LeetCod/index.js
+++ LeetCod/index.js
@@ -16,32 +16,69 @@ createHelloWorld();
 //inicialmente retorna  n e então retorna 1 a mais que o valor anterior
 //toda vez subsequente que é chamada ( n, n + 1, n + 2, etc).
 
-function counter(n){
-    let count = n;
+function counter(n) {
+  let count = n;
 
-    return function(){
-        return count++
-    }
+  return function () {
+    return count++;
+  };
 }
 
-let createCounter = counter(10)
+let createCounter = counter(10);
 
-console.log(createCounter())
-console.log(createCounter())
-console.log(createCounter())
-
-
+console.log(createCounter());
+console.log(createCounter());
+console.log(createCounter());
 
 // 2704. Ser ou não ser
 // Fácil
 // Empresas
-// Escreva uma função  expectque ajude os desenvolvedores a testar seus códigos. 
+// Escreva uma função  expect que ajude os desenvolvedores a testar seus códigos.
 // Ela deve receber qualquer valor  val e retornar um objeto com as duas funções a seguir.
 
-// toBe(val) aceita outro valor e retorna  true se os dois valores  === são iguais. 
+// toBe(val) aceita outro valor e retorna  true se os dois valores  === são iguais.
 // Se não forem iguais, deve lançar um erro  "Not Equal".
 
-// notToBe(val) aceita outro valor e retorna  true se os dois valores  !== são iguais. 
+// notToBe(val) aceita outro valor e retorna  true se os dois valores  !== são iguais.
 // Se forem iguais, deve lançar um erro  "Equal".
 
+function expect(val) {
+  return {
+      toBe: function(expected) {
+          if (val === expected) {
+              return true;
+          } else {
+              throw new Error("Not Equal");
+          }
+      },
+      notToBe: function(unexpected) {
+          if (val !== unexpected) {
+              return true;
+          } else {
+              throw new Error("Equal");
+          }
+      }
+  };
+}
+
+function runFunction(func) {
+  try {
+      const value = func();
+      return { value: value };
+  } catch (error) {
+      return { error: error.message };
+  }
+}
+
+
+let func1 = () => expect(5).toBe(5);
+console.log(runFunction(func1)); 
+
+
+let func2 = () => expect(5).toBe(null);
+console.log(runFunction(func2)); 
+
+
+let func3 = () => expect(5).notToBe(null);
+console.log(runFunction(func3)); 
 
